2ef54ef69be87331366d01aa59c8d48ca345d569
Ajay Gandhi <aag255@cornell.edu>
2015-07-27 10:05:19 -0400

    Update package, use passing build of node-speakable, update google-tts module

--- app.js
+++ app.js
@@ -7,10 +7,7 @@
 
 var program   = require('commander'),
     Speakable = require('speakable'),
-    GoogleTTS = require('google-tts'),
-    fs        = require('fs'),
-    Lame      = require('lame'),
-    Speaker   = require('speaker');
+    GoogleTTS = require('google-tts');
 
 program
   .version('1.0.0')
@@ -21,14 +18,9 @@ var stt = new Speakable({ key: 'AIzaSyD-tp0pAAegIAr3hk7BHhaja_PNkQFzsmc' });
 
 stt.on('speechResult', function(recognizedWords) {
   // Received result from Google, look for sage keyword
+  console.log('Text:', recognizedWords);
   if (recognizedWords.indexOf('Sage') >= 0) {
-    GoogleTTS.convert('Looking for me?', __dirname + '/temp', function () {
-      fs.createReadStream(__dirname + '/temp.mp3')
-        .pipe(new Lame.Decoder())
-        .on('format', function (format) {
-          this.pipe(new Speaker(format));
-        });
-    });
+    GoogleTTS.play('Looking for me?');
   }
   stt.recordVoice();
 });
